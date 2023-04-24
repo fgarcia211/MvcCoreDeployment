b58dbd2ac4d8ec9bd65f6da766a40538c2c0d625
fgarcia211 <fergarcia002100@outlook.es>
2023-04-24 14:22:21 +0200

    Add project files.

---
+++ MvcCoreDeployment/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreDeployment.Models;
+using System.Diagnostics;
+
+namespace MvcCoreDeployment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["SALUDO"] = "Welcom";
+            Random rnd = new Random();
+            List<int> numeros = new List<int>();
+
+            for (int i=1; i <= 10;i++)
+            {
+                numeros.Add(rnd.Next(1, 100));
+            }
+            return View(numeros);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
